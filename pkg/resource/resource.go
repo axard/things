1698d711bb2c7ae791733eb35f8d361db20d1580
Andrey Tarasov <andrey13.tarasov@yandex.ru>
2020-11-30 11:10:12 +0300

    Add package for work with module path

---
+++ pkg/resource/resource.go
@@ -0,0 +1,19 @@
+package resource
+
+import (
+	"path"
+	"strings"
+)
+
+func Package(resource string) string {
+	d, f := path.Split(resource)
+	return d + strings.TrimSuffix(f, path.Ext(f))
+}
+
+func Object(resource string) string {
+	return strings.TrimLeft(path.Ext(resource), ".")
+}
+
+func Validate(resource string) bool {
+	return Object(resource) != ""
+}
---
+++ pkg/resource/resource_test.go
@@ -0,0 +1,139 @@
+package resource
+
+import (
+	"testing"
+)
+
+func TestPackage(t *testing.T) {
+	type args struct {
+		resource string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Package() returns valid path if resource is full and valid",
+			args: args{
+				resource: "/path/to/package.Resource",
+			},
+			want: "/path/to/package",
+		},
+		{
+			name: "Package() returns valid path if resource has only delimeter dot without object name",
+			args: args{
+				resource: "/path/to/package.",
+			},
+			want: "/path/to/package",
+		},
+		{
+			name: "Package() returns valid path if resource hasn't object",
+			args: args{
+				resource: "/path/to/package",
+			},
+			want: "/path/to/package",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Package(tt.args.resource); got != tt.want {
+				t.Errorf("Package() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestObject(t *testing.T) {
+	type args struct {
+		resource string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Object() returns valid object name if resource is full and valid",
+			args: args{
+				resource: "/path/to/package.Resource",
+			},
+			want: "Resource",
+		},
+		{
+			name: "Object() returns empty string if resource has only delimeter dot without object name",
+			args: args{
+				resource: "/path/to/package.",
+			},
+			want: "",
+		},
+		{
+			name: "",
+			args: args{
+				resource: "/path/to/package",
+			},
+			want: "",
+		},
+		{
+			name: "",
+			args: args{
+				resource: ".Resource",
+			},
+			want: "Resource",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Object(tt.args.resource); got != tt.want {
+				t.Errorf("Object() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidate(t *testing.T) {
+	type args struct {
+		resource string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "",
+			args: args{
+				resource: "/path/to/package.Resource",
+			},
+			want: true,
+		},
+		{
+			name: "",
+			args: args{
+				resource: "/path/to/package.",
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				resource: "/path/to/package",
+			},
+			want: false,
+		},
+		{
+			name: "",
+			args: args{
+				resource: ".Resource",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Validate(tt.args.resource); got != tt.want {
+				t.Errorf("Validate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
